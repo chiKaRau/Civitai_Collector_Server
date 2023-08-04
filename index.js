f00b931d20ff0e9b3b075564796dee06565abe69
chiKaRau <Tencchi@hotmail.com>
2023-08-03 17:40:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -87,30 +87,3 @@ async function start() {
 }
 
 start()
-
-/*
-async function example() {
-  // To use ESM in CommonJS, you can use a dynamic import like this:
-  const { ChatGPTUnofficialProxyAPI } = await import('chatgpt')
-  const { oraPromise } = await import('ora')
-
-  // You can also try dynamic importing like this:
-  // const importDynamic = new Function('modulePath', 'return import(modulePath)')
-  // const { ChatGPTAPI } = await importDynamic('chatgpt')
-
-  const api = new ChatGPTUnofficialProxyAPI({
-    "accessToken":"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJycmV4YXVAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWV9LCJodHRwczovL2FwaS5vcGVuYWkuY29tL2F1dGgiOnsidXNlcl9pZCI6InVzZXItRjNrcWt2NmpHR1RoeGJDMVBCaDJ0VTkyIn0sImlzcyI6Imh0dHBzOi8vYXV0aDAub3BlbmFpLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExMDE0NjU5MTY4NTcxMTU4Njg5MiIsImF1ZCI6WyJodHRwczovL2FwaS5vcGVuYWkuY29tL3YxIiwiaHR0cHM6Ly9vcGVuYWkub3BlbmFpLmF1dGgwYXBwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2ODk5OTIxNTYsImV4cCI6MTY5MTIwMTc1NiwiYXpwIjoiVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEciLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIG1vZGVsLnJlYWQgbW9kZWwucmVxdWVzdCBvcmdhbml6YXRpb24ucmVhZCBvcmdhbml6YXRpb24ud3JpdGUgb2ZmbGluZV9hY2Nlc3MifQ.vG0JDp_m_CwAEGlVQXQqlgEe-uX-S8FfkuxFlRKn48mMYp2J09fca21er_3iG9-7cK4CYLluggycVSeLywZPSn16AxHeEhGulK0RVWdXmYWSmEOnZj4AqdKgQURDltRKmP0eOsKB2uaQSxqzc_0-VqjQQq1iP8oePvBwUNv0iGEuOiP90utxnSF5x1v3El9n8bpTfE4nVCqF06mkj1nHoAn9SY0pcAcXPCS92HfR6LdwTvAh7N60YZcsXcEZl_kDEA_t8F2XvpWcoj78RZILVQWdLYo5IDfnXBx3jtu7uDgbzCfDLMk1TWRU-IyQ6F_luMtJGCGIaWEeZ0AF86FkGA"
-  })
-
-  const prompt = 'Hello'
-
-  let res = await oraPromise(api.sendMessage(prompt), {
-    text: prompt
-  })
-
-  console.log('\n' + res.text + '\n')
-}
-
-
-example()
-*/
